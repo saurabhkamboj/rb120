2d0aeb75475854b640280f4ff5fe24082585fbde
Saurabh Kamboj <skamboj97@gmail.com>
2023-05-12 16:36:33 +0530

    Modify file

--- lesson_2/oo_rps.rb
+++ lesson_2/oo_rps.rb
@@ -22,23 +22,41 @@
 =end
 
 class Player
+  attr_accessor :move, :name
+
   def initialize(player_type)
     @player_type = player_type
     @move = nil
+    set_name
+  end
+
+  def set_name
+    if human?
+      print "What should we call you? "
+
+      loop do
+        self.name = gets.chomp
+
+        break unless self.name.empty?
+        print "Error! You must enter a value: "
+      end
+    else
+      self.name = 'Computer'
+    end
   end
 
   def choose
     if human?
-      print "Choose between rock, paper or scissors: "
+      print "Choose between rock, paper or scissor: "
 
       loop do
-        @move = gets.chomp
+        self.move = gets.chomp.downcase
 
         break if ['rock', 'paper', 'scissor'].include?(@move)
         puts "Error! Please input a valid choice."
       end
     else
-      @move = ['rock', 'paper', 'scissors'].sample
+      self.move = ['rock', 'paper', 'scissor'].sample
     end
   end
 
@@ -51,6 +69,7 @@ class RPSGame
   attr_accessor :human, :computer
 
   def initialize
+    display_welcome_message
     @human = Player.new(:human)
     @computer = Player.new(:computer)
   end
@@ -60,34 +79,49 @@ class RPSGame
   end
 
   def display_goodbye_message
-    puts "Thank you for playing."
+    puts "Thank you for playing #{human.name.capitalize}."
+  end
+
+  def display_winner
+    puts "You chose #{human.move.capitalize}."
+    puts "The computer chose #{computer.move.capitalize}."
+    puts result(human.move, computer.move)
+  end
+
+  def win?(move_1, move_2)
+    (move_1 == 'rock' && move_2 == 'scissor') ||
+      (move_1 == 'scissor' && move_2 == 'paper') ||
+      (move_1 == 'paper' && move_2 == 'rock')
+  end
+
+  def result(human_move, computer_move)
+    if win?(human_move, computer_move)
+      "You won!"
+    elsif win?(computer_move, human_move)
+      "Computer won!"
+    else
+      "It's a tie."
+    end
+  end
+
+  def play_again?
+    print "Do you want to play again? "
+    input = gets.chomp.downcase
+
+    ['n', 'no'].any?(input)
   end
 
   def play
-    display_welcome_message
-    human.choose
-    computer.choose
-    # display_winner
-    # display_goodbye_message
+    loop do
+      human.choose
+      computer.choose
+      display_winner
+
+      break if play_again?
+    end
+
+    display_goodbye_message
   end
-end
-
-class Move
-  def initialize
-    # seems like we need something to keep track
-    # of the choice... a move object can be "paper", "rock" or "scissors"
-  end
-end
-
-class Rule
-  def initialize
-    # not sure what the "state" of a rule object should be
-  end
-end
-
-# not sure where "compare" goes yet
-def compare(move1, move2)
-
 end
 
 RPSGame.new.play
