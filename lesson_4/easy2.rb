9fd4a50a8a4dcb53dd8ac5c2fab5eaafa1944760
Saurabh Kamboj <skamboj97@gmail.com>
2023-05-22 15:56:37 +0530

    Modify file

---
+++ lesson_4/easy2.rb
@@ -0,0 +1,58 @@
+# Question 4
+
+# What could you add to this class to simplify it and remove two methods from the class definition while still maintaining the same functionality?
+
+class BeesWax
+  def initialize(type)
+    @type = type
+  end
+
+  def type
+    @type
+  end
+
+  def type=(t)
+    @type = t
+  end
+
+  def describe_type
+    puts "I am a #{@type} of Bees Wax"
+  end
+end
+
+# Solution
+
+class BeesWax
+  attr_accessor :type
+
+  def initialize(type)
+    @type = type
+  end
+
+  def describe_type
+    puts "I am a #{type} of Bees Wax"
+  end
+end
+
+# Question 7
+
+# If we have a class such as the one below:
+
+class Cat
+  @@cats_count = 0
+
+  def initialize(type)
+    @type = type
+    @age  = 0
+    @@cats_count += 1
+  end
+
+  def self.cats_count
+    @@cats_count
+  end
+end
+
+# Explain what the @@cats_count variable does and how it works. What code would you need to write to test your theory?
+
+sparkle = Cat.new('example')
+p Cat.cats_count # => 1
